fa954311f9d1391c430b7b873b0c16fb45e51226
KOBAYASHI Hiroaki <hkoba@altair.localdomain>
2011-01-28 00:28:23 +0900

    Cosmetic cleansing. method 順番並べなおしなど。

--- scanadf.tcl
+++ scanadf.tcl
@@ -4,11 +4,9 @@ package require snit
 package require BWidget
 
 # XXX: Focus management.
-# XXX: Shift-Return
 
-# XXX: gthum preview
+# XXX: gthumb preview
 # XXX: note saving.
-# XXX: Cancel button
 
 # XXX: -filefmt, -dirfmt combobox, pwd entry
 # XXX: paper size selection/saving
@@ -16,8 +14,6 @@ package require BWidget
 # XXX: tooltip
 
 snit::widget scanadf {
-    component myScannerList
-
     option -verbose 0
     option -debug no
 
@@ -30,30 +26,6 @@ snit::widget scanadf {
     option -dirfmt %Y%m%d-%H:%M
     option -filefmt p%03d.pnm
 
-    component myMessage
-    proc gridrow {rowVar widget gridOpts args} {
-	upvar 1 $rowVar row
-	if {![info exists row]} {
-	    set row 0
-	} else {
-	    incr row
-	}
-	set widgetList {}
-	set optList {}
-	foreach {widget gridOpts} [linsert $args 0 $widget $gridOpts] {
-	    lappend widgetList $widget
-	    lappend optList $gridOpts
-	}
-	grid {*}$widgetList
-	for {set col 0} {$col < [llength $optList]} {incr col} {
-	    set widget [lindex $widgetList $col]
-	    set gridOpts [lindex $optList $col]
-	    if {[llength $gridOpts]} {
-		grid configure $widget -row $row -column $col \
-		    {*}$gridOpts
-	    }
-	}
-    }
     constructor args {
 	$self setup menu
 
@@ -143,10 +115,6 @@ snit::widget scanadf {
 	$m add cascade -label Debug -menu [menu $m.debug]
 	$m.debug add command -label Console -command [list $self console]
     }
-    method console {} {
-	package require tclreadline
-	after idle tclreadline::Loop
-    }
     method {setup message} {} {
 	foreach {tag style} {
 	    ok {-foreground blue}
@@ -158,13 +126,13 @@ snit::widget scanadf {
 	}
     }
 
+    #========================================
     option -device fujitsu
     option -source "ADF Duplex"
 
     option -i 1
     option -end ""
-    variable myPageHist [list]
-    component myPageHistFrm
+    variable myExpectedPages ""
     variable myTask ""
     variable curBook ""
     method newbook {} {
@@ -172,6 +140,112 @@ snit::widget scanadf {
 	$self history clear
 	set curBook [clock format [clock seconds] -format $options(-dirfmt)]
     }
+    method Scan {mode} {
+	if {$myTask ne ""} return
+
+	if {$curBook eq ""} {
+	    $self newbook
+	}
+	if {![file exists $curBook]} {
+	    file mkdir $curBook
+	}
+	set cmd [list scanadf \
+		     --device $options(-device) \
+		     --source $options(-source) \
+		     {*}[$self opt resolution] \
+		     --mode $options(-mode) \
+		     {*}[$self opt paper] \
+		     -o $curBook/$options(-filefmt) \
+		     -s $options(-i) \
+		]
+	$self history add
+	switch $mode {
+	    NPages {
+		set end [set options(-end) \
+				    [expr {$options(-i) + $options(-npages)
+					   - 1}]]
+	    }
+	    UntilEnd {
+		set end $options(-end)
+	    }
+	    default {
+		error "Invalid Scan mode($mode)!"
+	    }
+	}
+	set myExpectedPages [expr {$end - $options(-i) + 1}]
+	lappend cmd -e $end 2>@1
+	$self dputs cmd=$cmd
+	set myTask [open [list | {*}$cmd]]
+	fileevent $myTask readable [list $self readable $myTask]
+	$self Cancel active
+    }
+    method Cancel {{state ""}} {
+	if {$state ne ""} {
+	    $myPageHistFrm.cancel configure -state $state
+	} elseif {$myTask ne ""} {
+	    exec kill [pid $myTask]
+	}
+    }
+
+    method Error line {
+	after idle [list $self Finish]
+	$self emit $line error
+	error $line
+    }
+    method Finish {} {
+	$self history update
+	if {[catch {close $myTask} error]} {
+	    $self emit $error\n error
+	}
+	set myTask ""; # assert {$myTask eq $chan}
+    }
+
+    method {opt resolution} {} {
+	list --resolution 100 --y-resolution 100
+    }
+    method {opt paper} {} {
+	list -x 130 -y 190 --page-width 130 --page-height 190
+    }
+
+    #----------------------------------------
+    method readable chan {
+	set rc [catch {
+	    if {[eof $chan]} {
+		$self emit <EOF> warn
+		$self Finish
+	    } else {
+		gets $chan line
+		if {[regexp {^scanadf: (.*)} $line -> msg]} {
+		    $self emit $line\n warn
+		} elseif {[regexp {^Scanned document (.*)} $line -> fpath]} {
+		    scan [file tail $fpath] $options(-filefmt) num
+		    set options(-i) [expr {$num + 1}]
+		    $self emit $line\n ok
+		} elseif {[regexp {^Scanned (\d+) pages} $line -> pages]} {
+		    if {$pages == $myExpectedPages} {
+			$self emit $line\n done
+		    } else {
+			$self emit $line\n error
+		    }
+		    $self Finish
+		} elseif {[regexp jammed $line]} {
+		    $self Error $line\n
+		} else {
+		    $self emit $line\n
+		}
+	    }
+	} error]
+	if {$rc} {
+	    if {[info exists line]} {
+		after idle [list $self emit $line\n warn]
+	    }
+	    after idle [list $self emit $error\n error]
+	    after idle [list error $error]
+	}
+    }
+    #========================================
+    variable myPageHist [list]
+    component myPageHistFrm
     method {history clear} {} {
 	set myPageHist ""
 	$self history update
@@ -218,73 +292,8 @@ snit::widget scanadf {
 	    $myPageHistFrm.$w configure -state $state
 	}
     }
-    method Cancel {{state ""}} {
-	if {$state ne ""} {
-	    $myPageHistFrm.cancel configure -state $state
-	} elseif {$myTask ne ""} {
-	    exec kill [pid $myTask]
-	}
-    }
-    variable myExpectedPages ""
-    method Scan {mode} {
-	if {$myTask ne ""} return
-
-	if {$curBook eq ""} {
-	    $self newbook
-	}
-	if {![file exists $curBook]} {
-	    file mkdir $curBook
-	}
-	set cmd [list scanadf \
-		     --device $options(-device) \
-		     --source $options(-source) \
-		     {*}[$self opt resolution] \
-		     --mode $options(-mode) \
-		     {*}[$self opt paper] \
-		     -o $curBook/$options(-filefmt) \
-		     -s $options(-i) \
-		]
-	$self history add
-	switch $mode {
-	    NPages {
-		set end [set options(-end) \
-				    [expr {$options(-i) + $options(-npages)
-					   - 1}]]
-	    }
-	    UntilEnd {
-		set end $options(-end)
-	    }
-	    default {
-		error "Invalid Scan mode($mode)!"
-	    }
-	}
-	set myExpectedPages [expr {$end - $options(-i) + 1}]
-	lappend cmd -e $end 2>@1
-	$self dputs cmd=$cmd
-	set myTask [open [list | {*}$cmd]]
-	fileevent $myTask readable [list $self readable $myTask]
-	$self Cancel active
-    }
-
-    method {opt resolution} {} {
-	list --resolution 100 --y-resolution 100
-    }
-    method {opt paper} {} {
-	list -x 130 -y 190 --page-width 130 --page-height 190
-    }
-
-    method Error line {
-	after idle [list $self Finish]
-	$self emit $line error
-	error $line
-    }
-    method Finish {} {
-	$self history update
-	if {[catch {close $myTask} error]} {
-	    $self emit $error\n error
-	}
-	set myTask ""; # assert {$myTask eq $chan}
-    }
+    #========================================
+    component myMessage
     method emit {msg {tag ""} args} {
 	$myMessage insert end $msg $tag {*}$args
 	if {[$myMessage get end-1c] ne "\n"} {
@@ -292,49 +301,44 @@ snit::widget scanadf {
 	}
 	$myMessage see end
     }
-    method readable chan {
-	set rc [catch {
-	    if {[eof $chan]} {
-		$self emit <EOF> warn
-		$self Finish
-	    } else {
-		gets $chan line
-		if {[regexp {^scanadf: (.*)} $line -> msg]} {
-		    $self emit $line\n warn
-		} elseif {[regexp {^Scanned document (.*)} $line -> fpath]} {
-		    scan [file tail $fpath] $options(-filefmt) num
-		    set options(-i) [expr {$num + 1}]
-		    $self emit $line\n ok
-		} elseif {[regexp {^Scanned (\d+) pages} $line -> pages]} {
-		    if {$pages == $myExpectedPages} {
-			$self emit $line\n done
-		    } else {
-			$self emit $line\n error
-		    }
-		    $self Finish
-		} elseif {[regexp jammed $line]} {
-		    $self Error $line\n
-		} else {
-		    $self emit $line\n
-		}
-	    }
-	} error]
-	if {$rc} {
-	    if {[info exists line]} {
-		after idle [list $self emit $line\n warn]
-	    }
-	    after idle [list $self emit $error\n error]
-	    after idle [list error $error]
-	}
-    }
-    method myvar vn {
-	myvar $vn
-    }
 
     method dputs args {
 	$myMessage insert end $args\n
 	if {! $options(-verbose)} return
 	puts stderr $args
+    }
+
+    method console {} {
+	package require tclreadline
+	after idle tclreadline::Loop
+    }
+
+    method myvar vn {
+	myvar $vn
+    }
+    #========================================
+    proc gridrow {rowVar widget gridOpts args} {
+	upvar 1 $rowVar row
+	if {![info exists row]} {
+	    set row 0
+	} else {
+	    incr row
+	}
+	set widgetList {}
+	set optList {}
+	foreach {widget gridOpts} [linsert $args 0 $widget $gridOpts] {
+	    lappend widgetList $widget
+	    lappend optList $gridOpts
+	}
+	grid {*}$widgetList
+	for {set col 0} {$col < [llength $optList]} {incr col} {
+	    set widget [lindex $widgetList $col]
+	    set gridOpts [lindex $optList $col]
+	    if {[llength $gridOpts]} {
+		grid configure $widget -row $row -column $col \
+		    {*}$gridOpts
+	    }
+	}
     }
 }
 
