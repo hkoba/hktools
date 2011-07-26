81a41e7eb40dd5a5e001d181573d54b9ddacb2a3
KOBAYASI, Hiroaki <buribullet@gmail.com>
2011-07-26 18:58:19 +0900

    RPMDepSort.pm -- 'perl' subcommand returns perl centric dependencies.

--- sysadm/RPMDepSort.pm
+++ sysadm/RPMDepSort.pm
@@ -1,11 +1,16 @@
 #!/usr/bin/perl -w
+package RPMDepSort;
 use strict;
 use warnings FATAL => qw(all);
+sub MY () {__PACKAGE__}
+
 use RPM2;
 use Data::Dumper;
 
 {
-  package PkgDep;
+  sub PkgDep () {'RPMDepSort::PkgDep'}
+  package RPMDepSort::PkgDep;
+  sub PkgDep () {__PACKAGE__}
   use fields qw(pkg pkgName provide require whatrequires);
   sub new {
     my $class = shift;
@@ -25,6 +30,7 @@ use Data::Dumper;
     # Remove self dependency.
     foreach my $k (keys %{$self->{provide}}) {
       delete $self->{require}{$k};
+      delete $self->{whatrequires}{$k};
     }
     # Remove rpmlib(*) dependency too.
     foreach my $k (grep {/^rpmlib\(/} keys %{$self->{require}}) {
@@ -34,28 +40,26 @@ use Data::Dumper;
   }
 }
 
-#========================================
-
-my $deps = build_dependency(my $db = RPM2->open_rpm_db
-			   , sub {
-			     ! /-debuginfo$/
-			   });
-# print Dumper($deps);
-
-
-print join("\t", '#nreq', 'name', 'whatrequires...'), "\n";
-foreach my PkgDep $dep (sort_by_nreqs($deps)) {
-  my @whreqs = @{$dep->{whatrequires}};
-  print join("\t", scalar @whreqs, $dep->{pkg}->name, @whreqs), "\n";
+sub sort_by_whatrequires {
+  my ($self, $deps) = @_;
+  sort {
+    keys %{$a->{whatrequires}} <=> keys %{$b->{whatrequires}}
+      ||    $a->{pkgName} cmp $b->{pkgName}
+  } values %$deps;
 }
 
-#========================================
-
-sub sort_by_nreqs {
-  my ($deps) = @_;
+sub sort_by_requires {
+  my ($self, $deps) = @_;
   sort {
-    @{$a->{whatrequires}} <=> @{$b->{whatrequires}}
-#      ||    keys(%{$a->{require}}) <=> keys(%{$b->{require}})
+    keys(%{$a->{require}}) <=> keys(%{$b->{require}})
+      ||    $a->{pkgName} cmp $b->{pkgName}
+  } values %$deps;
+}
+
+sub sort_by_provides {
+  my ($self, $deps) = @_;
+  sort {
+    keys(%{$a->{provide}}) <=> keys(%{$b->{provide}})
       ||    $a->{pkgName} cmp $b->{pkgName}
   } values %$deps;
 }
@@ -71,7 +75,7 @@ sub list_nondeps {
 }
 
 sub build_dependency {
-  my ($db, $filter_match) = @_;
+  my ($db, $filter_match, $fixup) = @_;
 
   my %deps;
   my $all = $db->find_all_iter;
@@ -85,15 +89,70 @@ sub build_dependency {
     $deps{$pkgName} = my PkgDep $dep
       = PkgDep->new($pkg, $pkgName, $pkg->provides);
     $dep->add_requires($pkg->requires);
-    $dep->fixup;
   }
 
   foreach my PkgDep $dep (values %deps) {
-    $dep->{whatrequires} = [map {
-      $_->name
-    } $db->find_by_requires($dep->{pkgName})];
+    foreach my $prov (keys %{$dep->{provide}}) {
+      $dep->{whatrequires}{$_} = 1 for map {
+	$_->name
+      } $db->find_by_requires($prov);
+    }
+  }
+
+  foreach my PkgDep $dep (values %deps) {
+    $dep->fixup;
+    $fixup->($dep) if $fixup;
   }
 
   \%deps;
 }
 
+#========================================-
+
+sub emit_deps {
+  my ($pack, $deps) = @_;
+  print join("\t", '#nreq', 'name', 'whatrequires...'), "\n";
+  foreach my PkgDep $dep ($pack->sort_by_whatrequires($deps)) {
+    my @whreqs = sort keys %{$dep->{whatrequires}};
+    print join("\t", scalar @whreqs, $dep->{pkg}->name, @whreqs), "\n";
+  }
+}
+
+sub cmd_whatrequires {
+  my ($pack) = @_;
+  my $deps = build_dependency(my $db = RPM2->open_rpm_db
+			      , sub {
+				! /-debuginfo$/
+			      });
+  $pack->emit_deps($deps);
+}
+
+sub cmd_perl {
+  my ($pack) = @_;
+  my $deps = build_dependency(my $db = RPM2->open_rpm_db
+			      , sub {
+				/^perl-/ && !/-debuginfo$/
+			      }
+			      , \&perl_fixup
+			     );
+  $pack->emit_deps($deps);
+}
+
+sub perl_fixup {
+  (my PkgDep $dep) = @_;
+  foreach my $k (grep {/^perl\((?::MODULE_COMPAT[\w\.]+)\)/}
+		 keys %{$dep->{require}}) {
+    delete $dep->{require}{$k};
+  }
+}
+
+#========================================
+unless (caller) {
+  my $cmd = shift || 'whatrequires';
+  my $sub = MY->can("cmd_$cmd")
+    or die "No such command: $cmd\n";
+
+  $sub->(MY, @ARGV);
+}
+
+1;
