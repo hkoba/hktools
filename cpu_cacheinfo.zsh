f963685eb3e0216590495785b188d20e13ef6aba
KOBAYASI, Hiroaki <buribullet@gmail.com>
2011-11-10 11:53:06 +0900

    cpu_cacheinfo.zsh -- human friendly dump of /sys/devices/system/cpu/cpu0/cache/*

---
+++ cpu_cacheinfo.zsh
@@ -0,0 +1,33 @@
+#!/bin/zsh
+
+sysfs=/sys/devices/system/cpu
+cpu=cpu0
+
+function read_var_from {
+    local from=$1 var=$2
+    typeset -x "cacheinfo[$var]"=$(< $from/$var)
+}
+
+keys=(
+    coherency_line_size
+    number_of_sets
+    ways_of_associativity
+    level
+    type
+    size
+)
+
+typeset -A cacheinfo
+
+for n in $sysfs/$cpu/cache/index<0->; do
+    cacheinfo=()
+    for v in $keys; do
+	read_var_from $n $v
+    done
+    print -n L$cacheinfo[level]-$cacheinfo[type]
+    print -n " " $cacheinfo[size] " = "
+    print -n $cacheinfo[number_of_sets]sets "* "
+    print -n $cacheinfo[ways_of_associativity]ways "* "
+    print -n $cacheinfo[coherency_line_size]bytes/line
+    print
+done
