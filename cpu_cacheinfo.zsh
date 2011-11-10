eed109760848408c962eff8ba42246c312d34348
KOBAYASI, Hiroaki <buribullet@gmail.com>
2011-11-10 12:46:42 +0900

    cpu_cacheinfo.zsh -- For private/shared.

--- cpu_cacheinfo.zsh
+++ cpu_cacheinfo.zsh
@@ -3,6 +3,7 @@
 sysfs=/sys/devices/system/cpu
 cpu=cpu0
 
+typeset -A cacheinfo
 function read_var_from {
     local from=$1 var=$2
     typeset -x "cacheinfo[$var]"=$(< $from/$var)
@@ -15,9 +16,8 @@ keys=(
     level
     type
     size
+    shared_cpu_list
 )
-
-typeset -A cacheinfo
 
 grep '^model name' /proc/cpuinfo|head -1
 
@@ -27,7 +27,13 @@ for n in $sysfs/$cpu/cache/index<0->; do
 	read_var_from $n $v
     done
     print -n L$cacheinfo[level]-$cacheinfo[type]
-    print -n " " $cacheinfo[size] " = "
+    print -n " " $cacheinfo[size]
+    if [[ $cacheinfo[shared_cpu_list] != 0 ]]; then
+	print -n ", shared($cacheinfo[shared_cpu_list])"
+    else
+	print -n ", private"
+    fi
+    print -n $'\t=\t'
     print -n $cacheinfo[number_of_sets]sets "* "
     print -n $cacheinfo[ways_of_associativity]ways "* "
     print -n $cacheinfo[coherency_line_size]bytes/line
