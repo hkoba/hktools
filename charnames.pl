60919194e25d2a032764d975a38d8fd39432da24
KOBAYASI, Hiroaki <buribullet@gmail.com>
2013-07-22 16:15:28 +0900

    charnames.pl -- toy script to get Unicode character names of input chars.

---
+++ charnames.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/env perl
+use strict;
+use warnings FATAL => qw/all/;
+
+use open IO => ":locale";
+
+require charnames;
+
+while (<>) {
+  chomp;
+  foreach my $c (split //) {
+    if ($c =~ /^\s$/) {
+      print "\n"; next;
+    }
+    printf "%s\t\\x{%x}\t%s\n", $c, ord($c), charnames::viacode(ord($c));
+  }
+}
