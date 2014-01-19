98d13ba1030f52e17215af3a7f7d6f9768b7f006
KOBAYASI, Hiroaki <buribullet@gmail.com>
2014-01-19 18:39:48 +0900

    dd-uboot.zsh - toy script for uboot image extraction/installation.

---
+++ dd-uboot.zsh
@@ -0,0 +1,52 @@
+#!/bin/zsh
+
+set -e
+function die { echo 1>&2 $*; exit 1}
+
+function usage {
+    ((! ARGC)) || echo 1>&2 $*;
+    cat 1>&2 <<EOF; exit 1
+Usage: ${progfile:t} [-r | -w] DEV [IMAGE]
+EOF
+}
+
+function x {
+    print -- "$@"
+    if (($#o_dryrun)); then
+	return
+    fi
+    "$@"
+}
+
+#----------------------------------------
+
+dd_seek=2
+dd_cf=(
+    bs=512 count=592
+)
+
+function cmd--r {
+    local dev=$1; shift
+    local opt; ((ARGC)) && opt+=(of=$1)
+    x dd if=$dev $opt $dd_cf skip=$dd_seek
+}
+
+function cmd--w {
+    local dev=$1; shift
+    local opt; ((ARGC)) && opt+=(if=$1)
+    x dd of=$dev $opt $dd_cf seek=$dd_seek
+}
+
+#----------------------------------------
+
+zparseopts -D -K n=o_dryrun
+
+((ARGC)) && [[ $1 == -* ]] || usage
+
+cmd=cmd-$1; shift
+
+zparseopts -D -K n=o_dryrun
+
+(($+functions[$cmd])) || usage "Unknown command: $cmd"
+
+$cmd "$@"
