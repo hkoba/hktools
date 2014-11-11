7f74990fa5d35820172d403185ca8ecb88508742
KOBAYASI, Hiroaki <buribullet@gmail.com>
2014-11-11 12:10:31 +0900

    Add toy impl of iptables-apply

---
+++ sysadm/iptables-apply.zsh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+config=/etc/sysconfig/iptables
+: ${TIMEOUT:=15}
+
+function revert-unless-confirmed {
+  local saved=$1
+
+  echo Applying new rules...
+  service iptables restart
+
+  {
+     read -q -t $TIMEOUT "ret?Can you establish NEW connections to the machine? (y/N) "
+  } always {
+     if (($? > 0)); then
+       echo -n reverting...
+       iptables-restore < $saved
+       echo done
+     fi
+  }
+}
+
+revert-unless-confirmed =(iptables-save)
