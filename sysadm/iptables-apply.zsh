2694a7f534d42f3238bbe15f2e4da1a3e1699e83
KOBAYASI, Hiroaki <buribullet@gmail.com>
2014-11-11 14:10:26 +0900

    Add autoload is-at-least.

--- sysadm/iptables-apply.zsh
+++ sysadm/iptables-apply.zsh
@@ -6,26 +6,27 @@ config=/etc/sysconfig/iptables
 : ${TIMEOUT:=15}
 
 function revert-unless-confirmed {
-  local saved=$1
+    local saved=$1
 
-  echo Applying new rules...
-  service iptables restart
+    echo Applying new rules...
+    service iptables restart
 
-  local ret="" read_opts=()
-  if is-at-least 5.0; then
-    read_opts+=(-q)
-  fi
-  {
-     read $read_opts -t $TIMEOUT "ret?Can you establish NEW connections to the machine? (y/N) "
-  } always {
-     if (($? == 0)) && [[ $ret = y || $ret = Y ]]; then
-       echo ok.
-     else
-       echo -n reverting...
-       iptables-restore < $saved
-       echo done
-     fi
-  }
+    local ret="" read_opts=()
+    autoload is-at-least
+    if is-at-least 5.0; then
+	read_opts+=(-q)
+    fi
+    {
+	read $read_opts -t $TIMEOUT "ret?Can you establish NEW connections to the machine? (y/N) "
+    } always {
+	if (($? == 0)) && [[ $ret = y || $ret = Y ]]; then
+	    echo ok.
+	else
+	    echo -n reverting...
+	    iptables-restore < $saved
+	    echo done
+	fi
+    }
 }
 
 revert-unless-confirmed =(iptables-save)
