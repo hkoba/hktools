495482ef63a25f59a79c6cb90f3a26b6103915f3
KOBAYASI, Hiroaki <buribullet@gmail.com>
2016-03-04 12:29:20 +0900

    tsvcut.pl - [-u |--unix] (use LF for input/output line terminator)
    * Default is CRLF

--- tsvcut.pl
+++ tsvcut.pl
@@ -3,46 +3,61 @@
 use strict;
 use warnings FATAL => qw/all/;
 use autodie;
+use Getopt::Long;
 
 {
-  my %watchColNames;
-  while (@ARGV and $ARGV[0] =~ /^[\+\@](.*)$/) {
+  GetOptions("u|unix" => \ (my $o_unix = 0))
+    or usage();
+
+  my (%watchColNames, @watchColList);
+  while (@ARGV and $ARGV[0] =~ /^\@(.*)$/) {
     $watchColNames{$1}++;
+    push @watchColList, $1;
     shift @ARGV;
   }
   unless (keys %watchColNames) {
     die "Please specifly column name!\n";
   }
+
+  local ($/, $\) = map {$_, $_} $o_unix ? "\n" : "\r\n";
+
   foreach my $fn (@ARGV) {
-    local $/ = "\r\n";
     open my $fh, '<', $fn;
     defined(my $header = <$fh>)
       or do { warn "Can't read header from $fn\n"; next };
+    $header =~ s/^\xef\xbb\xbf//; # Trim BOM
     my @header = split "\t", $header;
-    my %found;
-    my @watchCols = map {
-      if ($watchColNames{$header[$_]}) {
-	$found{$header[$_]}++;
-	$_;
-      } else {
-	();
+    my @watchCols = do {
+      my %found;
+      foreach (0 .. $#header) {
+	defined $watchColNames{$header[$_]}
+	  or next;
+	$found{$header[$_]} = $_;
       }
-    } 0 .. $#header;
 
-    unless (keys %found == keys %watchColNames) {
-      die "Can't find column: "
-	.join(" ", grep {not $found{$_}} sort keys %watchColNames);
-    }
+      unless (keys %found == keys %watchColNames) {
+	die "Can't find column: "
+	  .join(" ", grep {not $found{$_}} sort keys %watchColNames);
+      }
 
-    print tsv(@header[@watchCols]), "\r\n";
+      map {$found{$_}} @watchColList;
+    };
+
+    print tsv(@header[@watchCols]);
 
     while (my $line = <$fh>) {
       my @cols = split "\t", $line;
-      print tsv(@cols[@watchCols]), "\r\n";
+      print tsv(@cols[@watchCols]);
     }
   }
 }
 
 sub tsv {
   join "\t", map {$_ // ''} @_;
+}
+
+sub usage {
+  die <<END;
+Usage: $0 [-u] file...
+END
 }
