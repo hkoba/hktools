8d922b08a275896897910967aac08d519ffd8347
KOBAYASI, Hiroaki <buribullet@gmail.com>
2016-03-04 12:30:43 +0900

    tsvheader.pl - Auto strip BOM and CR

--- tsvheader.pl
+++ tsvheader.pl
@@ -5,6 +5,8 @@ use warnings FATAL => qw/all/;
 
 {
   chomp(my $line = <>);
+  $line =~ s/^\xef\xbb\xbf//; # Trim BOM
+  $line =~ s/\r//;
   my $i = 0;
   print map($i++."\t".$_."\n", split "\t", $line);
 }
