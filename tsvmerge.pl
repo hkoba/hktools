9b9fce342f2bdeab130e2b47b454caf62aefa588
KOBAYASI, Hiroaki <buribullet@gmail.com>
2016-10-18 19:36:17 +0900

    Add an option to specify joining column.

--- tsvmerge.pl
+++ tsvmerge.pl
@@ -2,13 +2,36 @@
 use strict;
 use warnings FATAL => qw/all/;
 use autodie;
+use File::Basename;
 
 sub tsv (@);
+sub usage {
+  die <<END;
+Usage: @{[basename($0)]} [-N | +N] TSV_FILE...
+Merge TSV_FILEs by -N-th column value.
+- Default merge column is -1 (last column).
+- Each TSV_FILE must have header line.
+END
+}
+
 {
+  usage() unless @ARGV;
+
+  my $join_col = -1;
   my %all;
   my @header;
-  my $fileno = 0;
-  foreach my $fn (@ARGV) {
+  my $fileno = -1;
+  while (@ARGV) {
+    my $arg = shift @ARGV;
+
+    # Set $join_col when [-N | +N] is given.
+    if ($arg =~ /^([-+]\d+)$/) {
+      $join_col = $1;
+      next;
+    }
+
+    $fileno++;
+    my $fn = $arg;
     open my $fh, "<", $fn;
     local $_;
     push @header, do {
@@ -18,13 +41,11 @@ sub tsv (@);
     while (<$fh>) {
       chomp;
       my $tsv = [split "\t", $_];
-      $all{$tsv->[-1]}[$fileno] = $tsv;
+      $all{$tsv->[$join_col]}[$fileno] = $tsv;
     }
-  } continue {
-    $fileno++;
   }
 
-  print tsv(my @joined_header = map {@$_} @header);
+  print tsv(map {@$_} @header);
 
   foreach my $key (sort keys %all) {
     my $item = $all{$key};
