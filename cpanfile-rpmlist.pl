171aa4cc6c5d82f8f7e9cf166c06c32756b48901
KOBAYASI, Hiroaki <buribullet@gmail.com>
2016-12-19 16:18:02 +0900

    cpanfile-rpmlist.pl - Add packager filter.

--- cpanfile-rpmlist.pl
+++ cpanfile-rpmlist.pl
@@ -5,8 +5,13 @@ use warnings;
 use Getopt::Long;
 use Module::CPANfile;
 
+# cpanfile-rpmlist.pl -s --packager='Fedora' ./cpanfile
+
 {
-  GetOptions("ignore-file=s", \ (my $o_ignore_file))
+  GetOptions("ignore-file=s", \ (my $o_ignore_file)
+             , "s|silent", \ (my $o_silent)
+             , "packager=s", \ (my $o_packager)
+           )
     or die "Invalid options!";
   my %ignored = map {$_=>1} qw/perl/;
   if ($o_ignore_file) {
@@ -20,7 +25,19 @@ use Module::CPANfile;
     my $req = Module::CPANfile->load($cpanfile)->prereq_specs;
     foreach my $mod (map {map {sort keys %$_} @{$req->{$_}}{qw/requires recommends/}} qw(runtime test)) {
       next if $ignored{$mod};
-      print "perl($mod)\n";
+      my $dep = "perl($mod)";
+      if ($o_packager) {
+        my $pkg = qx(rpm -q --whatprovides --qf '%{PACKAGER}' '$dep');
+        if ($? != 0) {
+          print STDERR "Not in rpm, skipped: $mod\n" unless $o_silent;
+          next;
+        }
+        if ($pkg !~ /$o_packager/) {
+          print STDERR "Packager ($pkg) doesn't match, skipped: $mod\n" unless $o_silent;
+          next;
+        }
+      }
+      print "$dep\n";
     }
   }
 }
