a30007dcd4caf304454fd6c33427497416e89dde
Kobayasi, Hiroaki <buribullet@gmail.com>
2017-07-16 19:22:10 +0900

    Add mindots

---
+++ minsetup/200dots/run.zsh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+source $0:h:h/config.env
+thisDir=$(cd $0:h && print $PWD)
+
+zparseopts -D -K n=o_dryrun
+
+((ARGC)) || die "Usage: $0 [-n] HOST"
+
+destHost=$1; shift
+
+mindotsDir=$toolRootDir:h/mindots
+
+[[ -d $minDotsDir ]] || die "Can't find mindots"
+
+echo "#" Copying $minDotsDir/ to $destHost
+x rsync -Cavz $minDotsDir/ $destHost:$minDotsDir:t
+
+echo "#" Setting up mindots
+x ssh -t $destHost $minDotsDir/setup.zsh
