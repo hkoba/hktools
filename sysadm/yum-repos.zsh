c40129b7c1e03314cec9b255fa06fbdbe6b4dce6
Kobayasi, Hiroaki <buribullet@gmail.com>
2017-07-22 15:06:11 +0900

    Add support for ignore

--- sysadm/yum-repos.zsh
+++ sysadm/yum-repos.zsh
@@ -9,10 +9,20 @@ set -e
 o_outfn=(-o -)
 zparseopts -D -K v=o_verbose o:=o_outfn
 
+#----------------------------------------
+typeset -A ignored
+{
+    for x in $argv; do
+        ignored[$x:t:r]=1
+    done
+}
+#----------------------------------------
+
 cd /etc/yum.repos.d
 
 repos=()
 for fn in *.repo; do
+    ((! $+ignored[$fn:t:r])) || continue
     rpm -qf $fn >&/dev/null && continue
     repos+=($fn)
 done
