4191234501337d359bbdd387e5c4c572880c9d18
Kobayasi, Hiroaki <buribullet@gmail.com>
2018-04-16 17:49:22 +0900

    Rewrite to add `--history-only` option.

--- git-kconv-fast-import.pl
+++ git-kconv-fast-import.pl
@@ -2,20 +2,50 @@
 use strict;
 use Jcode;
 
-my $last_pos;
-while (<>) {
-  if (my ($origBytes) = /^data (\d+)/ and $1) {
-    unless (read(ARGV, $_, $origBytes)) {
-      die $!;
-    }
-    $_ = Jcode->new($_)->utf8;
-    my $newBytes = length($_);
-    print "data $newBytes\n";
-    print STDERR "Changed data at $. ($origBytes => $newBytes)";
-  }
-  print;
-} continue {
-  $last_pos = tell ARGV;
+use Getopt::Long qw/:config no_ignore_case/;
+
+sub usage {
+  die <<END;
+Usage: $0  [-H | --history-only] ...FILES...
+Convert legacy kanji code to utf8 in git fast-import files.
+END
+
 }
 
+GetOptions("H|history-only", \ (my $o_history_only),
+           "h|help", \ (my $o_help),
+           "d|debug+", \ (my $o_debug = 0),
+         )
+  or usage();
 
+$o_help and usage();
+
+{
+  while (<>) {
+    if ($o_history_only) {
+      if (my $line = /^commit / .. (my ($origBytes) = /^data (\d+)/)) {
+        $_ = read_n_convert($origBytes) if $line =~ /E0$/ and $origBytes;
+      }
+    } else {
+      if (my ($origBytes) = /^data (\d+)/) {
+        $_ = read_n_convert($origBytes) if $origBytes;
+      }
+    }
+    print;
+  }
+}
+
+sub read_n_convert {
+  my ($origBytes) = @_;
+  my $buf = "";
+  unless (read(ARGV, $buf, $origBytes)) {
+    die $!;
+  }
+  my $converted = Jcode->new($buf)->utf8;
+  my $newBytes = length($converted);
+  print STDERR "# Changed data at $. ($origBytes => $newBytes)\n" if $o_debug;
+  if ($o_debug >= 2) {
+    print STDERR "-- $_\n" for split /\n/, $converted;
+  }
+  "data $newBytes\n$converted";
+}
