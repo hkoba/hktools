65622c23fbd50f81bee624a2999aa9d9f493a1d7
Kobayasi, Hiroaki <buribullet@gmail.com>
2019-01-08 14:53:06 +0900

    script -> runnable module with submethods

--- web/slashbar/SlashBar.pm
+++ web/slashbar/SlashBar.pm
@@ -4,6 +4,31 @@ use strict;
 use File::AddInc;
 use MOP4Import::Base::CLI_JSON -as_base;
 
+sub parse_sample_url {
+  (my MY $self, my $sample_url) = @_;
+
+  unless ($sample_url) {
+    $self->cmd_help("Not enough argument! parse_sample_url /SAMPLE/URL/-/LIKE/THIS\n");
+  }
+
+  my ($appPrefix, $innerPath) = $sample_url =~ m{^(.*?)/-(/.*)$}s
+    or $self->cmd_help("Sample URL must contain '/-/'!");
+
+  my @els = split m{([^/]+)}, $innerPath;
+  my $i = 0;
+  my $re = "(?<s$i>".shift(@els).")";
+  while (@els) {
+    shift @els;
+    my $word = q{\w+};
+    $re = "(?<w$i>$re(?:$word)?)";
+    my $slash = shift @els or next;
+    $re = "(?<s$i>$re(?:$slash)?)";
+  } continue {
+    $i++;
+  }
+  $re;
+}
+
 MY->run(\@ARGV) unless caller;
 
 1;
