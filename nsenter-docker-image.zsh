c535930022129a69b96ade75d0caaa3ab2a07dfe
Kobayasi, Hiroaki <buribullet@gmail.com>
2020-02-12 16:32:37 +0900

    minor improvement

--- nsenter-docker-image.zsh
+++ nsenter-docker-image.zsh
@@ -6,22 +6,29 @@ scriptFn=$0
 
 function usage {
     cat 1>&2 <<EOF
-Usage: ${scriptFn:t} IMAGE_NAME
+Usage: ${scriptFn:t} IMAGE_NAME [COMMAND...]
 EOF
     exit 1
 }
+
+function die { echo 1>&2 $*; exit 1 }
 
 #----------------------------------------
 
 ((ARGC)) || usage
 
-imageName=$1
+imageName=$1; shift
+((ARGC)) || argv=(/bin/bash)
 
 cid=$(docker container ls -q --filter ancestor=$imageName) || return 1
 
+[[ -n $cid ]] || die "Can't find container for image $imageName"
+
 pid=$(docker inspect --format '{{.State.Pid}}' $cid) || return 1
+
+[[ -n $pid ]] || die "Can't find pid for container $cid"
 
 sudo=()
 [[ -r /proc/$pid/ns ]] || sudo=(sudo)
 
-$sudo nsenter --target $pid --mount --uts --ipc --net --pid /bin/bash || return 1
+$sudo nsenter --target $pid --mount --uts --ipc --net --pid "$argv[@]" || return 1
