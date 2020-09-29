c88daf1917ba391c24e674adea121ef75f67fd14
Kobayasi, Hiroaki <hkoba@ssri.com>
2020-09-29 12:03:06 +0900

    Add submodule recursion

--- git-remote-subst.zsh
+++ git-remote-subst.zsh
@@ -6,22 +6,32 @@ setopt err_return
 progname=$0:t
 
 function usage {
-  cat <<EOF
-Usage: $progname FROM TO
+  cat 1>&2 <<EOF
+Usage: $progname [-n] [-q] [-C DIR] FROM TO
 EOF
-  exit
+  exit 1
 }
 
 function x {
   (($#o_quiet))  || print -- "#" "$@"
   (($#o_dryrun)) && return
-  "$@"
+  "$@" || return $?
 }
 
-o_dryrun=() o_quiet=()
-zparseopts -D -K n=o_dryrun q=o_quiet
+o_dryrun=() o_quiet=() o_chdir=()
+zparseopts -D -K n=o_dryrun q=o_quiet C:=o_chdir
 
 ((ARGC == 2)) || usage
+
+# -C ディレクトリ名
+# で、↑このディレクトリに cd してから動作
+if (($#o_chdir)); then
+    cd $o_chdir
+fi
+
+if ((! $#o_quiet)); then
+    print PWD=$PWD
+fi
 
 from=$1
 to=$2
@@ -34,7 +44,8 @@ for remote in $(git remote); do
 
     new_remote=${remoteUrl/$~from/$to}
 
-    x git remote set-url $remote $new_remote
+    x git remote set-url $remote $new_remote || break
 
 done
 
+git submodule foreach --recursive $0 $o_dryrun $o_quiet $from $to
