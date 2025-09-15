f609c0fd9c54d1483fee9249f959f540b06042ac
Kobayasi, Hiroaki <buribullet@gmail.com>
2025-09-15 17:33:20 +0900

    Use rl_json

--- find-conpromised-npm.tcl
+++ find-conpromised-npm.tcl
@@ -1,6 +1,8 @@
 #!/usr/bin/tclsh
 
-set conpromisedDict {
+package require fileutil
+
+set ::conpromisedDict {
 backslash	0.2.1
 chalk-template	1.1.1
 supports-hyperlinks	4.1.1
@@ -21,18 +23,65 @@ debug	4.4.2
 ansi-styles	6.2.2
 }
 
-set pipe [open [list | find -name package.json | xargs jq -r {[.name, .version] | @tsv}]]
-
-while {[gets $pipe line] >= 0} {
-    if {![llength $line]} continue
-    # puts stderr "# $line"
-    lassign $line package version
-    if {![dict exists $conpromisedDict $package]} continue
-    if {[set ngVer [dict get $conpromisedDict $package]] eq $version} {
-        puts "NG $package"
+proc report {package version} {
+    if {![dict exists $::conpromisedDict $package]} return
+    set ngVer [dict get $::conpromisedDict $package]
+    set vcomp [package vcompare $version $ngVer]
+    if {$vcomp == 0} {
+        return [list NG $package]
     } else {
-        puts "ok $package ($ngVer <=> $version: [package vcompare $ngVer $version])"
+        set msg [expr {$vcomp > 0 ? "newer" : "older"}]
+        return [list ok $package "$version: $msg than $ngVer"]
     }
 }
 
-close $pipe
+if {[catch {
+    package require rl_json
+    namespace import rl_json::*
+}]} {
+    
+    set pipe [open [list | find -name package.json | xargs jq -r {[.name, .version] | @tsv}]]
+
+    while {[gets $pipe line] >= 0} {
+        if {![llength $line]} continue
+        # puts stderr "# $line"
+        lassign $line package version
+        set diag [report $package $version]
+        if {$diag ne ""} {
+            puts $diag
+        }
+    }
+
+
+} else {
+    set pipe [open [list | find -name package.json]]
+
+    array set stats [list ok 0 NG 0]
+
+    while {[gets $pipe fn] >= 0} {
+        set json [fileutil::cat $fn]
+        set diag ""
+        try {
+            if {![json exists $json name] || ![json exists $json version]} continue
+            set package [json get $json name]
+            set version [json get $json version]
+            if {[set diag [report $package $version]] ne ""} {
+                puts $diag\t$fn
+            }
+
+            incr stats([lindex $diag 0])
+
+        } on error err {
+            puts "ERR $err\t$fn"
+        }
+    }
+
+    puts ""
+    foreach k {ok NG} {
+        puts "$k $stats($k)"
+    }
+}
+
+if {$pipe ne ""} {
+    close $pipe
+}
